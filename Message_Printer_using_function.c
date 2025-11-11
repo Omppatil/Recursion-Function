04d6cfd53c8d8fe0ec1fda4dd41f0508a86cc6cb
Om Patil <omppatil2002@gmail.com>
2025-11-11 12:28:47 +0530

    New C code added

---
+++ Message_Printer_using_function.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void greet();
+
+int main(void) {
+  printf("Welcome to message printer\n");
+  greet();
+  return 0;
+}
+
+void greet() {
+  printf("Hello world!\n");
+  printf("My name is OMI");
+}
